83180685b95f9fd00303dad875c6fe1f657e82f3
Nigel Schuster <nigel.schusters@googlemail.com>
2017-04-04 19:03:16 -0400

    JS5

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,11 +1,11 @@
 var Generator = require('yeoman-generator');
 
-module.exports = class extends Generator {
-  method1() {
+module.exports = Generator.extend({
+  method1: function() {
     this.fs.copyTpl(
       this.templatePath('index.html'),
       this.destinationPath('index.html'),
       { title: "FB Stack" }
     );
   }
-};
+});
