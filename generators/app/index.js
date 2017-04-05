f6162c0d9c29cf0282ca8e784d839110bb36ef1c
Nigel Schuster <nigel.schusters@googlemail.com>
2017-04-04 23:11:34 -0400

    Hopefully finally a working build for travis

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,7 +1,15 @@
 var Generator = require('yeoman-generator');
+var path = require('path');
 
 module.exports = class extends Generator {
   prompting() {
+    if(this.option('headless')) {
+      this.answers = {
+        title: 'FB Stack',
+        graphql: true
+      }
+      return;
+    }
     return this.prompt([{
       type: 'input',
       name: 'title',
@@ -13,7 +21,10 @@ module.exports = class extends Generator {
       message: 'Create graphl endpoint',
       default: true
     }]).then((answers) => {
-      this.composeWith('fb-fullstack:html-page', {title: answers.title});
+      this.answers = answers;
     });
+  }
+  writing() {
+    this.composeWith(path.join(__dirname, '../html-page/index.js'), {title: this.answers.title});
   }
 }
